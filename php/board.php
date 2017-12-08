25ab433f4b2e3df92aad2ca3c6d4bad5d764cd7b
felixxseol <felixxseol@gmail.com>
2017-12-08 20:41:27 +0000

    ???

---
+++ php/board.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once("./dbconfig.php");	
+$div = $_POST["div"];
+if($div == "start") {
+
+	$query = "select * from board";
+	$result = $conn->query($query);
+	$arr = array();
+	while($row = $result->fetch_assoc()) {
+		$arr[] = $row;
+	}
+	
+	echo json_encode($arr);
+	//print_r($arr);
+}
+if($div == "news"){
+	$no = $_POST["no"];
+	
+	$query = "select * from board where no = ".$no;
+	$result = $conn->query($query);
+	$arr = array();
+	while($row = $result->fetch_assoc()) {
+		$arr[] = $row;
+	}
+
+	echo json_encode($arr);
+}
+?>
